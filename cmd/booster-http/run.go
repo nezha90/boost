3fe2e4193960fa6d4188e1e7631ca4986fa3a70d
nezha <nezhayoung@outlook.com>
2025-01-17 15:37:31 +0800

    加日志

--- cmd/booster-http/run.go
+++ cmd/booster-http/run.go
@@ -313,7 +313,7 @@ var runCmd = &cli.Command{
 			}
 
 			fmt.Printf("all cars: %v\n", cars)
-			fmt.Printf("开始加载 car")
+			fmt.Printf("开始加载 car, %s\n", time.Now().String())
 			loader := NewLoader(os.Stderr)
 			multicar := frisbii.NewMultiReadableStorage()
 			var wg sync.WaitGroup
@@ -335,7 +335,7 @@ var runCmd = &cli.Command{
 				return err
 			}
 
-			fmt.Printf("加载 car 结束")
+			fmt.Printf("加载 car 结束, %s\n", time.Now())
 			loader.SetStatus("Loaded CARs, starting server ...")
 
 			opts.Multicar = multicar
